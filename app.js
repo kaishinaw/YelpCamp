0e230620377b2de3b73d1c0be2094b7c97c742ba
kaishinaw <kaishinaw@gmail.com>
2018-02-02 20:35:12 -0500

    update dburl

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var User = require("./models/user");
 var methodOverride = require("method-override");
 var seedDB = require("./seeds");
 
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://Kai:yelpcamp@ds223578.mlab.com:23578/yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://Kai:yelpcamp@ds223578.mlab.com:23578/yelpcamp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
